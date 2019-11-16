e81ceababf1adc3926f09d36a7b00d26b2860cc2
mgpathi <mgpathi18888@gmail.com>
2019-11-17 00:18:48 +0530

    Connecting to local DB, adding created date for inserted record

--- models/course.model.js
+++ models/course.model.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
- 
+
 //Attributes of the Course object
 var courseSchema = new mongoose.Schema({
-courseName: {
-type: String,
-required: 'This field is required!'
-},
-courseId: {
-type: String
-},
-courseDuration: {
-type: String
-},
-courseFee: {
-type: String
-}
+    courseName: {
+        type: String,
+        required: 'This field is required!'
+    },
+    courseId: {
+        type: String
+    },
+    courseDuration: {
+        type: String
+    },
+    courseFee: {
+        type: String
+    },
+    createdDate: {
+        type: Date
+    }
 });
- 
+
 mongoose.model('Course', courseSchema);
