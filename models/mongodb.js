ef4040aa06a6b44419ca0ea88ba9fbe71229ed53
Ganapathi <ganapathi.marpina@otsi.co.in>
2019-11-16 16:54:55 +0530

    initial commit

---
+++ models/mongodb.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://rootuser:Mon8oCN2@192.168.1.143:1611/admin', {useNewUrlParser: true}, (err) => {
+if (!err) {
+console.log('Successfully Established Connection with MongoDB')
+}
+else {
+console.log('Failed to Establish Connection with MongoDB with Error: '+ err)
+}
+});
+ 
+//Connecting Node and MongoDB
+require('./course.model');
